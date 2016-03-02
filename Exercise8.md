7086e1cd6eba78fd4f8ab4fdbf83f93c39d9af78
nicolaigj <ngjellestad@gmail.com>
2016-03-02 22:38:29 +0100

    Fixed syntax error

---
+++ Exercise8.md
@@ -0,0 +1,10 @@
+# Exercise 8
+
+Create a script that accepts a csv with computer names and return a table with the following values:
+- Computer name
+- When the OS was installed
+- The MAC address of the network adapter
+
+Example run
+```powershell
+PS C:>.\Exercise8.ps1
--- test.ps1
+++ test.ps1
@@ -7,7 +7,7 @@ import-csv $csv | ForEach-Object {
     $properties = @{
         "ComputerName" = $_.DNSHostName
         "InstallDate" = (Get-CimInstance Win32_OperatingSystem -ComputerName $_.DNSHostName).InstallDate
-        "MAC" = (get-netadapter)ifName
+        "MAC" = (get-netadapter).ifName
     }
     $obj = New-Object -TypeName PSObject -Property $properties
     $obj | select-object -Property ComputerName,InstallDate,MAC | Export-Csv result.csv -Append
